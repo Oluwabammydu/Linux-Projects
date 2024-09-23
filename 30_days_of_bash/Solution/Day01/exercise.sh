a8ea35fdb4afddcbadceb3af0b4e0f2edf345ce2
Oluwabammydu ojo <oluwabamise77@gmail.com>
2024-09-23 16:33:21 +0000

    Adding Day01 tasks

---
+++ 30_days_of_bash/Solution/Day01/exercise.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir my_project
+cd my_project
+
+touch file1.txt file2.txt file3.txt
+ls 
+echo "Project setup complete!"
---
+++ 30_days_of_bash/Solution/Day01/hello_world.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ #Run this command to create the script and grant permission
+ #touch hello_world.sh && chmod +x hello_world.sh
+
+
+echo "Hello, World!"
+
+#Run your script:
+#./hello_world.sh
+
+
